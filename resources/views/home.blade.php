cb65ab09b17a81fdc92208ed28bc73fce2f42067
unknown <adrielugano@gmail.com>
2019-12-01 22:26:21 -0300

    Correções

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,119 @@
-@extends('layouts.app')
+@extends('layouts.header')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <label id="board-title">Quadro de Tarefas</label>
+                <button class="btn-create btn" data-toggle="modal" data-target="#novaListaModal">Nova lista
+                    <i class="fas fa-plus-square"></i>
+                </button>
+                <button class="btn-nova-tarefa btn-create btn" data-toggle="modal" data-target="#novaTarefaModal">Nova
+                    tarefa
+                    <i class="fas fa-plus-square"></i>
+                </button>
+            </div>
+            <div class="panel-body">
+                <div id="board" class="row">
+                    <div id="lanes">
+                        <div v-for="(lane,index) in lanes" class="scrolling-wrapper">
+                            <div id="a" class="">
+                                <lane-card :lane="lane"></lane-card>
+                                <div style="float:left;"><button type="button" class="btn-delete btn btn-danger btn-xs"
+                                        v-on:click="removeElement(index)"><i class="fas fa-trash-alt"></i></button>
+                                </div>
+                            </div>
                         </div>
-                    @endif
 
-                    You are logged in!
+                        <!-- MODAL CREATE LANE-->
+                        <div class="modal fade" id="novaListaModal" tabindex="-1" role="dialog"
+                            aria-labelledby="novaListaModalLabel">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal"
+                                            aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        <h4 class="modal-title" id="novaListaModalLabel">Adicionar Nova Lista</h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form>
+                                            <div class="form-group">
+                                                <label for="nomeLista" class="control-label">Nome da Lista:</label>
+                                                <input type="text" class="form-control" id="nomeLista" name="nomeLista"
+                                                    v-on:keypress.enter="addLane">
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default"
+                                            data-dismiss="modal">Fechar</button>
+                                        <button type="button" class="btn btn-submit" data-dismiss="modal"
+                                            v-on:click="addLane">Salvar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- MODAL CREATE LANE-->
+
+                        <!-- MODAL CREATE TASK-->
+                        <div class="modal fade" id="novaTarefaModal" tabindex="-1" role="dialog"
+                            aria-labelledby="novaTarefaModalLabel">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal"
+                                            aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        <h4 class="modal-title" id="novaTarefaModalLabel">Adicionar Nova Tarefa</h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form>
+                                            <div class="form-group">
+                                                <label for="nomeTarefa" class="control-label">Nome da Tarefa:</label>
+                                                <input type="text" class="form-control" id="nomeTarefa"
+                                                    name="nomeTarefa">
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="control-label">Inserir na Lista:</label><br>
+                                                <select id="id-lane" class="form-control">
+                                                    <option v-for="lane in lanes" :value=lane.id>@{{lane.title}}
+                                                    </option>
+                                                </select>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default"
+                                            data-dismiss="modal">Fechar</button>
+                                        <button type="button" class="btn btn-submit" data-dismiss="modal"
+                                            v-on:click="addTask">Salvar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- MODAL CREATE TASK-->
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    <script type="text/javascript">
+    $('#novaListaModal').on('show.bs.modal', function(event) {
+        var button = $(event.relatedTarget) // Button that triggewhite the modal
+        var recipient = button.data('whatever') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text('Adicionar Nova Lista')
+        modal.find('.modal-body input').val(recipient)
+    })
+
+    function deleteTask(value) {
+        $("#" + value).remove();
+    }
+    </script>
+
+    <script type="text/javascript" src="{{ URL::asset('js/dragAndDrop.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('js/lanesComponent.js') }}"></script>
+
+    @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,23 +1,30 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <title>Laravel</title>
+    
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/home.css') }}" >
+
+    <style>
+        .panel-default>.panel-heading {
+            
+            margin-bottom: 20px;
+        }
+        
+    </style>
 </head>
 <body>
     <div id="app">
@@ -76,5 +83,17 @@
             @yield('content')
         </main>
     </div>
+    <!-- JavaScripts -->
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    <script src="//cdnjs.cloudflare.com/ajax/libs/vue/2.5.2/vue.min.js"></script>
+    <!-- CDNJS :: Sortable (https://cdnjs.com/) -->
+    <script src="//cdn.jsdelivr.net/npm/sortablejs@1.8.4/Sortable.min.js"></script>
+    <!-- CDNJS :: Vue.Draggable (https://cdnjs.com/) -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/Vue.Draggable/2.20.0/vuedraggable.umd.min.js"></script>
+
+    @yield('content')
 </body>
 </html>
