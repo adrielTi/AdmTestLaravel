7c3990a53ddbf0dfd56719118b7e01175bba68f7
unknown <adrielugano@gmail.com>
2019-12-02 01:09:56 -0300

    retirando ;

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ Route::get('/', function () {
 })
 */
 
-;
 
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
+
